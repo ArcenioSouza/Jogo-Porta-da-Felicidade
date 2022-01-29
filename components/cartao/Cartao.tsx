9a1e51cc9e076fb499bafa1f22b8c358cf985502
Arcenio Souza <arcenio_neto@icloud.com>
2022-01-29 16:09:32 -0300

    Adiciona cartão na página index com função de iniciar o jogo

--- components/cartao/Cartao.tsx
+++ components/cartao/Cartao.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
-import styles from "../../styles/Cartao.module.css"
+import React, { Children } from "react";
+import styles from "../../styles/Cartao.module.css";
 
-const Cartao = (props) => {
-  return (
-      <div className={styles.cartao}>
+interface CartaoProps {
+   bgColor?: string;
+   children?: any;
+}
 
+const Cartao = (props: CartaoProps) => {
+   return (
+      <div
+         className={styles.cartao}
+         style={{ backgroundColor: props.bgColor ?? "#fff" }}
+      >
+         {props.children}
       </div>
-  );
+   );
 };
 
 export default Cartao;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,25 @@
+import Link from "next/link";
 import Cartao from "../components/cartao/Cartao";
+import styles from "../styles/Formulario.module.css"
 
-export default function Form() {
+export default function Formulario() {
   return (
-    <div style={{display: "flex"}}>
-      <Cartao/>
+    <div className={styles.formulario}>
+      <div>
+          <Cartao bgColor="#c0392c">
+            <h1>Monty Hall</h1>
+          </Cartao>
+          <Cartao></Cartao>
+      </div>
+      <div>
+        <Cartao></Cartao>
+          <Cartao bgColor="#28a085">
+            <Link href="jogo/10/2">
+              <h2 className={styles.link}>Iniciar</h2>
+            </Link>
+          </Cartao>
+      </div>
+      
     </div>
   )
 }
